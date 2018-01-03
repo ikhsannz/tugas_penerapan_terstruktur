1f56a325a1f29a32cef8345043ff456bf32abe0e
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-01-03 23:40:00 +0800

    matrix R*W

--- SAW.cpp
+++ SAW.cpp
@@ -37,4 +37,12 @@ cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
 	for(int i=0;i<5;i++){
 		cout << w[i]<<" | ";
 	}cout <<endl;
+		// Matrix R * W
+	cout <<endl<< "MATRIKS R * W : "<<endl; 
+	double RWac[3][5];
+	for(int i=0;i<3;i++){
+		for(int j=0;j<5;j++){
+			RWac[i][j]=Rac[i][j]*w[j]; cout <<RWac[i][j]<<" | ";
+		}cout <<endl;
+	}
 }
